397a4d960926c8d9d3f1b484c7702e0851a7cfdf
brockhaywood <brock.haywood@gmail.com>
2015-11-03 11:13:24 -0800

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
 setup(name='LitleSdkPython',
-      version='9.3.1',
+      version='9.3.2',
       description='Litle & Co. SDK for Python',
       author='Litle & Co',
       author_email='SDKSupport@litle.com',
